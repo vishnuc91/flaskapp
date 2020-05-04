e421d9d916b8a60127aa52221d0284a199e5fd91
vishnu <vishnucherumadathil@gmail.com>
2020-05-04 23:02:53 +0530

    added readme file

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # flaskapp
-Gets Sendor information from IOT Device
+
+This Python Flask backend application contains backend services which fetches data from IOT device. Service is created for fetching temperature data based on date and time. I have used Mongo Database and Flask to create backend services.
+
+### `Project Setup using Conda`
+
+1: Create anaconda environment by `conda create -n {environment_name} python=3.5`.
+
+2: Activate anaconda environment using `conda activate {environment_name}`.
+
+3: Clone the project and go to the project path.
+
+4: To install all the packages run `pip install -r requirements.txt`.
+
+5: Change MongoDB credentials inside `application.py`.
+
+6: Set Flask enviromental variable like `EXPORT FLASK_APP=application.py` and `EXPORT FLASK_DEBUG=1`.
+
+7: We can also run the project by simply running `python application.py`.
+
+### `Project Setup using Docker`
+
+1: Navigate to the project folder
+
+2: To build the image run the docker command  `sudo docker build --tag flaskapp .`.
+
+3: To Start the application using container run `sudo docker run --name python-app -p 5000:5000 flaskapp`.
+
+**Note: Enter data in database using post operation**
+
+Example: "http://localhost:5000/home"
+METHOD: POST
+
+sample json payload:{
+	"reading": 32.5,
+	"timestamp": 1506161234,
+	"sensorType": "Temperature"
+}
--- application.py
+++ application.py
@@ -5,7 +5,6 @@ from pymongo import MongoClient
 from flask_cors import CORS
 import urllib
 
-
 username = urllib.parse.quote_plus('admin')
 password = urllib.parse.quote_plus('Cr0$$t@b123')
 uri = "mongodb://{username}:{password}@localhost:27017/?authSource=admin".format(username=username, password=password)
@@ -31,7 +30,7 @@ class IOTSensor(Resource):
             sensordatas = sensordata.find({'date': {'$lt': to_date, '$gte': from_date}})
             for datas in sensordatas:
                 sensor_data.append({"temperature": datas["temperature"], "sensortype": datas["sensortype"],
-                   "date": datas["date"], "time": datas["time"]})
+                                    "date": datas["date"], "time": datas["time"]})
             if len(sensor_data) > 0:
                 return {'message': 'Successfull', 'data': sensor_data}
             else:
@@ -39,13 +38,14 @@ class IOTSensor(Resource):
         else:
             sensordatas = sensordata.find()
             # Used mongo aggregations to query for mean ,max and min
-            aggregate_data = sensordata.aggregate([{'$group': {'_id': 'none', 'min': {"$min":"$temperature"},
-                                                               "max": {"$max":"$temperature"}, "avg": {"$avg":"$temperature"}}}])
+            aggregate_data = sensordata.aggregate([{'$group': {'_id': 'none', 'min': {"$min": "$temperature"},
+                                                               "max": {"$max": "$temperature"},
+                                                               "avg": {"$avg": "$temperature"}}}])
             for data in aggregate_data:
                 aggregates = data
             for datas in sensordatas:
                 sensor_data.append({"temperature": datas["temperature"], "sensortype": datas["sensortype"],
-                   "date": datas["date"], "time": datas["time"]})
+                                    "date": datas["date"], "time": datas["time"]})
             return {'message': 'Successfull', 'data': sensor_data, 'aggregates': aggregates}
 
     # @api.doc(parser=parser)
@@ -53,8 +53,8 @@ class IOTSensor(Resource):
         json_data = request.get_json()
         date_time = datetime.fromtimestamp(json_data["timestamp"]).isoformat()
         date_time = datetime.strptime(date_time, "%Y-%m-%dT%H:%M:%S")
-        data= {"temperature": json_data["reading"], "sensortype": json_data["sensorType"],
-               "date": date_time.date().isoformat(), "time": date_time.time().isoformat()}
+        data = {"temperature": json_data["reading"], "sensortype": json_data["sensorType"],
+                "date": date_time.date().isoformat(), "time": date_time.time().isoformat()}
         sensor_data = sensordata.insert_one(data).inserted_id
         return {'message': 'Data Saved', "id": str(sensor_data)}
 
